40b4c23befde2817c17b69df5a2432fdf1e1d579
= <=>
2021-10-18 15:49:53 +0100

    lint

--- vqe_in_dft/mol_plot.py
+++ vqe_in_dft/mol_plot.py
@@ -6,6 +6,7 @@ from typing import List
 
 import numpy as np
 import py3Dmol
+
 # rdkit import is weird and requires each part to be imported before going deeper
 import rdkit
 from pyscf import gto
