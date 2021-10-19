b86e996ddd2309687bf18d12c5d2a752e96deddf
= <=>
2021-10-19 10:47:29 +0100

    test for qiskit

--- tests/test_ham_converter.py
+++ tests/test_ham_converter.py
@@ -1,23 +1,28 @@
 """Tests for the hamiltonian converter."""
 
-from vqe_in_dft import nbed
 from pathlib import Path
+
+import numpy as np
 from openfermion import QubitOperator
+from qiskit_nature.operators.second_quantization.spin_op import SpinOp
+
 from vqe_in_dft.ham_converter import HamiltonianConverter
 
 water_filepath = Path("tests/molecules/water.xyz").absolute()
 
-hamiltonian = (
-    0.5 * QubitOperator("") + 0.25 * QubitOperator("X2") + 0.2 * QubitOperator("Y3")
-)
-# TODO need to sort the qubit number issue.
+hamiltonian = 0.5 * QubitOperator("")
+hamiltonian += 0.25 * QubitOperator("X2")
+hamiltonian += 0.2 * QubitOperator("Y3")
+
+qiskit_hamiltonian = SpinOp([("IIII", 0.5), ("IIXI", 0.25), ("IIIY", 0.2)])
+
 
 def test_of_to_int() -> None:
     converted_ham = HamiltonianConverter(hamiltonian).convert("qiskit")
-    import pdb
-
-    pdb.set_trace()
+    assert type(converted_ham) is SpinOp
+    assert converted_ham.register_length == 4
+    assert np.all(converted_ham.to_matrix() - qiskit_hamiltonian.to_matrix() == 0)
 
 
 if __name__ == "__main__":
-    test_of_to_int()
+    pass
