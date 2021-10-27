20defbf628750a1076b28f1fc807651f4a15db01
= <=>
2021-10-27 23:23:21 +0100

    localized rks now in localizer class

--- nbed/localizers/base.py
+++ nbed/localizers/base.py
@@ -3,6 +3,7 @@
 import logging
 from abc import ABC, abstractmethod
 from typing import List, Optional, Tuple
+from cached_property import cached_property
 
 import numpy as np
 import scipy as sp
@@ -24,7 +25,7 @@ class Localizer(ABC):
     Mulliken charges. As a result, IBOs are always well-defined.  (Ref: J. Chem. Theory Comput. 2013, 9, 4834−4843)
 
     Args:
-        pyscf_scf (gto.Mole): PySCF molecule object
+        pyscf_rks (gto.Mole): PySCF molecule object
         n_active_atoms (int): Number of active atoms
         localization_method (str): String of orbital localization method (spade, pipekmezey, boys, ibo)
         occ_cutoff (float): Threshold for selecting occupied active region (only requried if
@@ -50,16 +51,16 @@ class Localizer(ABC):
 
     def __init__(
         self,
-        pyscf_scf: StreamObject,
+        pyscf_rks: StreamObject,
         n_active_atoms: int,
         occ_cutoff: Optional[float] = 0.95,
         virt_cutoff: Optional[float] = 0.95,
         run_virtual_localization: Optional[bool] = False,
     ):
 
-        if pyscf_scf.mo_coeff is None:
+        if pyscf_rks.mo_coeff is None:
             logger.debug("SCF method not initialised, running now...")
-            pyscf_scf.run()
+            pyscf_rks.run()
             logger.debug("SCF method initialised.")
 
         if self.output not in ["qiskit", "pennylane", "openfermion"]:
@@ -68,7 +69,7 @@ class Localizer(ABC):
             )
             config_valid = False
 
-        self._pyscf_scf = pyscf_scf
+        self._pyscf_rks = pyscf_rks
         self._n_active_atoms = n_active_atoms
         self._occ_cutoff = occ_cutoff
         self._virt_cutoff = virt_cutoff
@@ -76,6 +77,54 @@ class Localizer(ABC):
 
         # Run the localization procedure
         self.run()
+
+    @cached_property
+    def localized_rks(self) -> StreamObject:
+        """Localize the input RKS object."""
+
+        local_rks = self._global_rks
+
+        hcore_std = local_rks.get_hcore()
+        local_rks.get_hcore = (
+            lambda *args: self._local_basis_transform.conj().T
+            @ hcore_std
+            @ self._local_basis_transform
+        )
+
+        local_rks.get_veff = (
+            lambda mol=None, dm=None, dm_last=0, vhf_last=0, hermi=1: self._rks_veff(
+                local_rks, self._local_basis_transform, dm=dm, check_result=True
+            )
+        )
+
+        # overwrite C matrix with localised orbitals
+        local_rks.mo_coeff = self.localized_system.c_loc_occ_and_virt
+        dm_loc = local_rks.make_rdm1(
+            mo_coeff=local_rks.mo_coeff, mo_occ=local_rks.mo_occ
+        )
+
+        # fock_locbasis = _local_rks.get_hcore() + _local_rks.get_veff(dm=dm_loc)
+        fock_locbasis = local_rks.get_fock(dm=dm_loc)
+
+        # orbital_energies_std = _local_rks.mo_energy
+        orbital_energies_loc = np.diag(
+            local_rks.mo_coeff.conj().T @ fock_locbasis @ local_rks.mo_coeff
+        )
+        local_rks.mo_energy = orbital_energies_loc
+
+        # # check electronic energy matches standard global calc
+        # local_rks_total_energy_loc = local_rks.energy_tot(dm=dm_loc)
+        # if not np.isclose(self._local_rks.e_tot, local_rks_total_energy_loc):
+        #     raise ValueError(
+        #         "electronic energy of standard calculation not matching localized calculation"
+        #     )
+
+        # check if mo energies match
+        # orbital_energies_std = _local_rks.mo_energy
+        # if not np.allclose(orbital_energies_std, orbital_energies_loc):
+        #     raise ValueError('orbital energies of standard calc not matching localized calc')
+
+        return local_rks
 
     @abstractmethod
     def _localize(
@@ -104,10 +153,10 @@ class Localizer(ABC):
             raise ValueError("gamma_full != gamma_active + gamma_enviro")
 
         # check number of electrons is still the same after orbitals have been localized (change of basis)
-        s_ovlp = self._pyscf_scf.get_ovlp()
+        s_ovlp = self._pyscf_rks.get_ovlp()
         n_active_electrons = np.trace(self.dm_active @ s_ovlp)
         n_enviro_electrons = np.trace(self.dm_enviro @ s_ovlp)
-        n_all_electrons = self._pyscf_scf.mol.nelectron
+        n_all_electrons = self._pyscf_rks.mol.nelectron
         bool_flag_electron_number = np.isclose(
             (n_active_electrons + n_enviro_electrons), n_all_electrons
         )
@@ -121,7 +170,7 @@ class Localizer(ABC):
         """Localise virtual (unoccupied) orbitals using different localization schemes in PySCF.
 
         Args:
-            pyscf_scf (StreamObject): PySCF molecule object
+            pyscf_rks (StreamObject): PySCF molecule object
             n_active_atoms (int): Number of active atoms
             virt_cutoff (float): Threshold for selecting unoccupied (virtual) active regio
 
@@ -131,18 +180,18 @@ class Localizer(ABC):
             enviro_virtual_MO_inds (np.array): 1D array of environment virtual MO indices
         """
         logger.debug("Localizing virtual orbitals.")
-        n_occupied_orbitals = np.count_nonzero(self._pyscf_scf.mo_occ == 2)
-        c_std_occ = self._pyscf_scf.mo_coeff[:, :n_occupied_orbitals]
-        c_std_virt = self._pyscf_scf.mo_coeff[:, self._pyscf_scf.mo_occ < 2]
+        n_occupied_orbitals = np.count_nonzero(self._pyscf_rks.mo_occ == 2)
+        c_std_occ = self._pyscf_rks.mo_coeff[:, :n_occupied_orbitals]
+        c_std_virt = self._pyscf_rks.mo_coeff[:, self._pyscf_rks.mo_occ < 2]
 
         c_virtual_loc = vvo.vvo(
-            self._pyscf_scf.mol, c_std_occ, c_std_virt, iaos=None, s=None, verbose=None
+            self._pyscf_rks.mol, c_std_occ, c_std_virt, iaos=None, s=None, verbose=None
         )
 
-        ao_slice_matrix = self._pyscf_scf.mol.aoslice_by_atom()
+        ao_slice_matrix = self._pyscf_rks.mol.aoslice_by_atom()
 
         # TODO: Check the following:
-        # S_ovlp = pyscf_scf.get_ovlp()
+        # S_ovlp = pyscf_rks.get_ovlp()
         # S_half = sp.linalg.fractional_matrix_power(S_ovlp , 0.5)
         # C_loc_occ_ORTHO = S_half@C_loc_occ_full
         # run numerator_all and denominator_all in ortho basis
@@ -205,6 +254,6 @@ class Localizer(ABC):
             c_virtual = self._localize_virtual_orbs()
         else:
             # appends standard virtual orbitals from SCF calculation (NOT localized in any way)
-            c_virtual = self._pyscf_scf.mo_coeff[:, self._pyscf_scf.mo_occ < 2]
+            c_virtual = self._pyscf_rks.mo_coeff[:, self._pyscf_rks.mo_occ < 2]
 
         self.c_loc_occ_and_virt = np.hstack((self._c_loc_occ, c_virtual))
