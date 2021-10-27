ce49204975f9c3c4240fb6c589ae4ed865ad21d4
= <=>
2021-10-27 11:54:28 +0100

    exception when no embedding

--- nbed/exceptions.py
+++ nbed/exceptions.py
@@ -3,9 +3,15 @@ Custom Exceptions
 """
 
 
-class NbedConfigError(ValueError):
+class NbedConfigError(Exception):
     """
     Raised when config is not valid.
     """
 
+    pass
+
+class NbedDriverError(Exception):
+    """
+    Raise when NbedDriver finds itself in a bad state.
+    """
     pass
