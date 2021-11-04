73facd8ac61947f5950bf07469222c1af4bfab50
AlexisRalli <rallilex@hotmail.co.uk>
2021-11-04 15:26:54 +0000

    missing import in driver.py added

--- nbed/driver.py
+++ nbed/driver.py
@@ -18,7 +18,7 @@ from openfermion.ops.representations import get_active_space_integrals
 
 from nbed.exceptions import NbedConfigError
 
-from .localizers import BOYSLocalizer, IBOLocalizer, PMLocalizer, SPADELocalizer
+from .localizers import BOYSLocalizer, IBOLocalizer, PMLocalizer, SPADELocalizer, Localizer
 from .scf import huzinaga_RHF
 
 logger = logging.getLogger(__name__)
