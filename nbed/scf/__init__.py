2c49dc04804696751f79cbaa58d5a47896bc0066
Juan Francisco <juanfrh7@gmail.com>
2022-08-27 11:37:29 +0100

    change file names

--- nbed/scf/__init__.py
+++ nbed/scf/__init__.py
@@ -1,6 +1,6 @@
 """Localized SCF Methods."""
-from .embedded_hcore_funcs import energy_elec
+from .embedded_hcore_funcs import energy_elec, _absorb_h1e
 from .huzinaga_rhf import huzinaga_RHF
-from .huzinaga_rks import huzinaga_RKS
+from .huzinaga_ks import huzinaga_KS
 
-all = ["huzinaga_RHF", "huzinaga_RKS", "energy_elec"]
+all = ["huzinaga_RHF", "huzinaga_KS", "energy_elec", "_absorb_h1e"]
