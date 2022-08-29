343a7178ab34fdf58af533bcae2e1965c6a85de0
Juan Francisco <juanfrh7@gmail.com>
2022-08-29 13:09:51 +0100

    modify spinorb func to include open shells

--- nbed/ham_builder.py
+++ nbed/ham_builder.py
@@ -288,7 +288,7 @@ class HamiltonianBuilder:
             qubit_reduction += final_n_qubits - n_qubits
 
 def _spinorb_from_spatial(one_body_integrals, two_body_integrals):
-    n_qubits = one_body_integrals[0].shape[0] + one_body_integrals[1].shape[0]
+    n_qubits = one_body_integrals[0].shape[0] + one_body_integrals[1].shape[1] 
 
     # Initialize Hamiltonian coefficients.
     one_body_coefficients = np.zeros((n_qubits, n_qubits))
@@ -296,27 +296,22 @@ def _spinorb_from_spatial(one_body_integrals, two_body_integrals):
         (n_qubits, n_qubits, n_qubits, n_qubits))
 
     # Loop through integrals.
-    for p in range(n_qubits // 2):
+    for p in range(n_qubits // 2 ):
         for q in range(n_qubits // 2):
 
             # Populate 1-body coefficients. Require p and q have same spin.
             one_body_coefficients[2 * p, 2 * q] = one_body_integrals[0, p, q]
-            one_body_coefficients[2 * p, 2 * q] = one_body_integrals[1, p, q]
+            #one_body_coefficients[2 * p, 2 * q] = one_body_integrals[1, p, q]
 
-            one_body_coefficients[2 * p + 1, 2 * q +
-                                  1] = one_body_integrals[0, p, q]
+            #one_body_coefficients[2 * p + 1, 2 * q + 1] = one_body_integrals[0, p, q]
             one_body_coefficients[2 * p + 1, 2 * q +
                                   1] = one_body_integrals[1, p, q]
                                   
             # Continue looping to prepare 2-body coefficients.
-            for r in range(n_qubits // 2):
+            for r in range(n_qubits // 2 ):
                 for s in range(n_qubits // 2):
 
                     # Mixed spin
-                    two_body_coefficients[2 * p, 2 * q + 1, 2 * r + 1, 2 *
-                                          s] = (two_body_integrals[1][p, q, r, s])
-                    two_body_coefficients[2 * p + 1, 2 * q, 2 * r, 2 * s +
-                                          1] = (two_body_integrals[0][p, q, r, s])
                     two_body_coefficients[2 * p, 2 * q + 1, 2 * r + 1, 2 *
                                           s] = (two_body_integrals[0][p, q, r, s])
                     two_body_coefficients[2 * p + 1, 2 * q, 2 * r, 2 * s +
@@ -334,5 +329,6 @@ def _spinorb_from_spatial(one_body_integrals, two_body_integrals):
         np.absolute(one_body_coefficients) < EQ_TOLERANCE] = 0.
     two_body_coefficients[
         np.absolute(two_body_coefficients) < EQ_TOLERANCE] = 0.
+
 
     return one_body_coefficients, two_body_coefficients
