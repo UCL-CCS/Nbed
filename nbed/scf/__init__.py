3e368ed923b67fd08904e644fd0b7cb08cd473b1
MIWdlB <m.i.w@outlook.com>
2024-12-03 14:37:16 +0000

    use imports for scf.init all

--- nbed/scf/__init__.py
+++ nbed/scf/__init__.py
@@ -4,4 +4,4 @@ from .embedded_hcore_funcs import _absorb_h1e, energy_elec
 from .huzinaga_hf import huzinaga_HF
 from .huzinaga_ks import huzinaga_KS
 
-all = ["huzinaga_HF", "huzinaga_KS", "energy_elec", "_absorb_h1e"]
+all = [huzinaga_HF, huzinaga_KS, energy_elec, _absorb_h1e]
