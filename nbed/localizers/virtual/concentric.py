174b4760fadb8638bc873a72e6283d1dcff9fcb4
MIWdlB <m.i.w@outlook.com>
2025-05-21 13:41:33 +0100

    add missing mol init

--- nbed/localizers/virtual/concentric.py
+++ nbed/localizers/virtual/concentric.py
@@ -74,6 +74,7 @@ class ConcentricLocalizer(BaseVirtualLocalizer):
         projected_mol.basis = embedded_scf.mol.basis  # can be anything
         projected_mol.charge = embedded_scf.mol.charge
         projected_mol.spin = embedded_scf.mol.spin
+        projected_mol.build()
         projected_mf = scf.RKS(projected_mol)
         n_act_proj_aos = projected_mol.aoslice_by_atom()[self._n_active_atoms - 1][-1]
         logger.debug(f"{n_act_proj_aos=}")
