0a226ce278dec0d53e650a4fa720130c957f55ad
MIWdlB <m.i.w@outlook.com>
2025-08-21 14:00:46 +0100

    correct errors in pao normalisaton and diag cutoff

--- nbed/localizers/virtual/projected_atomic.py
+++ nbed/localizers/virtual/projected_atomic.py
@@ -5,7 +5,6 @@ import logging
 import numpy as np
 from numpy.typing import NDArray
 from pyscf.lib import StreamObject
-from scipy.linalg import fractional_matrix_power
 
 from nbed.localizers.virtual.base import VirtualLocalizer
 
@@ -106,9 +105,7 @@ def _localize_virtual_spin_pao(
     # Take the columns of C matrix (MOs)
     truncated_paos = pao_projector[:, np.abs(pao_norms) > norm_cutoff]
 
-    s_half = fractional_matrix_power(ao_overlap, 0.5)
-
-    renormalized_paos = s_half @ truncated_paos
+    renormalized_paos = truncated_paos
     renormalized_paos = renormalized_paos / np.sqrt(
         np.einsum("ij,ij->j", renormalized_paos, renormalized_paos)
     )
@@ -121,7 +118,7 @@ def _localize_virtual_spin_pao(
 
     logger.debug(f"{eigvecs.shape=}")
     # How to transform the truncated paos?
-    final_paos = renormalized_paos[:, eigvals > overlap_cutoff]
+    final_paos = renormalized_paos[:, np.abs(eigvals) > overlap_cutoff]
     logger.debug(f"{final_paos.shape=}")
 
     if (n_paos := final_paos.shape[-1]) == 0:
