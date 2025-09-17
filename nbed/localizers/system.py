55b9cf8b7216b4e59ba8696c851ffbed9417677b
MIWdlB <m.i.w@outlook.com>
2025-09-17 12:33:48 +0100

    add logging to localized system

--- nbed/localizers/system.py
+++ nbed/localizers/system.py
@@ -1,9 +1,12 @@
 """Class defining the data output from Localizers."""
 
+import logging
 from dataclasses import dataclass, field
 
-from numpy.typing import NDArray
 import numpy as np
+from numpy.typing import NDArray
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -38,15 +41,35 @@ class LocalizedSystem:
 
         # For spinless systems we need twice as many electrons
         # In a single density matrix.
-        self.dm_active *= 2 if self.dm_active.ndim==2 else 1
-        self.dm_enviro *= 2 if self.dm_enviro.ndim==2 else 1
-        self.dm_loc_occ *= 2 if self.dm_loc_occ.ndim==2 else 1
+        self.dm_active *= 2 if self.dm_active.ndim == 2 else 1
+        self.dm_enviro *= 2 if self.dm_enviro.ndim == 2 else 1
+        self.dm_loc_occ *= 2 if self.dm_loc_occ.ndim == 2 else 1
 
-    def from_spin_components(alpha: "LocalizedSystem", beta: "LocalizedSystem") -> "LocalizedSystem":
-        active_occ_inds=np.array([alpha.active_occ_inds, beta.active_occ_inds])
-        enviro_occ_inds=np.array([alpha.enviro_occ_inds, beta.enviro_occ_inds])
+        logger.debug("LocalizedSystem built.")
+        logger.debug(f"{self.active_occ_inds=}")
+        logger.debug(f"{self.enviro_occ_inds=}")
+        logger.debug(f"{self.c_active.shape=}")
+        logger.debug(f"{self.c_enviro.shape=}")
+        logger.debug(f"{self.c_loc_occ.shape=}")
+
+    def from_spin_components(
+        alpha: "LocalizedSystem", beta: "LocalizedSystem"
+    ) -> "LocalizedSystem":
+        """Construct a spin-aware LocalizedSystem from two spinless ones.
+
+        Args:
+            alpha (LocalizedSystem): The localized alpha spins
+            beta (LocalizedSystem): The localized beta spins.
+
+        Returns:
+            LocalizedSystem: A combined localized system with spins (alpha, beta).
+        """
+        active_occ_inds = np.array([alpha.active_occ_inds, beta.active_occ_inds])
+        enviro_occ_inds = np.array([alpha.enviro_occ_inds, beta.enviro_occ_inds])
         c_active = np.array([alpha.c_active, beta.c_active])
         c_enviro = np.array([alpha.c_enviro, beta.c_enviro])
         c_loc_occ = np.array([alpha.c_loc_occ, beta.c_loc_occ])
         c_loc_virt = np.array([alpha.c_loc_virt, beta.c_loc_virt])
-        return LocalizedSystem(active_occ_inds, enviro_occ_inds, c_active, c_enviro, c_loc_occ, c_loc_virt)
+        return LocalizedSystem(
+            active_occ_inds, enviro_occ_inds, c_active, c_enviro, c_loc_occ, c_loc_virt
+        )
